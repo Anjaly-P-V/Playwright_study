870350c0f175b0ae359d073fd63008e0c89ce682
Anjaly P V <anjalyviswambharan@gmail.com>
2024-08-19 16:32:50 +0530

    edited test_expedia_ui.py file

--- test_expedia/test_expedia_ui.py
+++ test_expedia/test_expedia_ui.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless = false)
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://www.expedia.co.in/")
